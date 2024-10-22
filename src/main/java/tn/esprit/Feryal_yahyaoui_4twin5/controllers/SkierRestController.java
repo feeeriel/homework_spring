e554b8412bb5493783be04b975d8246142fb6276
feeeriel <104622808+feeeriel@users.noreply.github.com>
2024-10-22 20:57:22 +0100

    Update SkierRestController.java

--- src/main/java/tn/esprit/Feryal_yahyaoui_4twin5/controllers/SkierRestController.java
+++ src/main/java/tn/esprit/Feryal_yahyaoui_4twin5/controllers/SkierRestController.java
@@ -2,16 +2,21 @@ package tn.esprit.Feryal_yahyaoui_4twin5.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 import tn.esprit.Feryal_yahyaoui_4twin5.entities.Skier;
+import tn.esprit.Feryal_yahyaoui_4twin5.services.ISkierServices;
 import tn.esprit.Feryal_yahyaoui_4twin5.services.SkierServicesImpl;
+
+import java.time.LocalDate;
+import java.util.Set;
 
 @RequiredArgsConstructor
 @RequestMapping("skier")
 @RestController
 public class SkierRestController {
-    @Autowired
-    private final SkierServicesImpl skierService;
+
+    private final ISkierServices skierService;
 
     @PostMapping("/add")
     public Skier saveSkier(@RequestBody Skier skier){
@@ -21,6 +26,12 @@ public class SkierRestController {
     public Skier getSkier(@PathVariable Long numSkier){
         return  skierService.retrieveSkier(numSkier);
     }
+    @DeleteMapping("/delete/{numSkier}")
+    public void deleteSkier(@PathVariable Long numSkier){
+         skierService.removeSkier(numSkier);
+    }
+    @PutMapping("/update")
+    public Skier updateSkier(@RequestBody Skier skier){
+        return skierService.updateSkier(skier);
+    }
 }
-
-
