c0d65df5b28c87cc478c242449b702a36cc532a1
feeeriel <104622808+feeeriel@users.noreply.github.com>
2024-10-22 20:56:06 +0100

    Update RegistrationRestController.java

--- src/main/java/tn/esprit/Feryal_yahyaoui_4twin5/controllers/RegistrationRestController.java
+++ src/main/java/tn/esprit/Feryal_yahyaoui_4twin5/controllers/RegistrationRestController.java
@@ -3,7 +3,9 @@ package tn.esprit.Feryal_yahyaoui_4twin5.controllers;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import tn.esprit.Feryal_yahyaoui_4twin5.entities.Piste;
 import tn.esprit.Feryal_yahyaoui_4twin5.entities.Registration;
+import tn.esprit.Feryal_yahyaoui_4twin5.entities.Skier;
 import tn.esprit.Feryal_yahyaoui_4twin5.services.RegistrationServicesImpl;
 
 @RequiredArgsConstructor
@@ -20,5 +22,14 @@ public class RegistrationRestController {
     public Registration getRegistration(@PathVariable Long numRegistration) {
         return registrationServices.retrieveRegistration(numRegistration);
     }
+    @DeleteMapping("/delete/{numRegistration}")
+    public void deleteSkier(@PathVariable Long numRegistration){
+        registrationServices.removeRegistration(numRegistration);
+    }
+    @PutMapping("/update")
+    public Registration updateRegistration(@RequestBody Registration registration) {
+        return registrationServices.updateRegistration(registration);
+    }
+
 }
 
